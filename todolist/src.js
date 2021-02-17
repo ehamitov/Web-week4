600fc1f86d2df5f1cf9e33629bf16867fc1b2062
ehamitov <60595562+ehamitov@users.noreply.github.com>
2021-02-17 23:05:04 +0500

    Update src.js

--- todolist/src.js
+++ todolist/src.js
@@ -17,27 +17,33 @@ for (i = 0; i < close.length; i++) {
     }
 }
 
-
-
-
 function Add() {
+    let checkbox = document.createElement('input');
+    checkbox.setAttribute("type", "checkbox");
+    checkbox.setAttribute("onclick", "checkItem(this)");
     var li = document.createElement("li");
     var inputValue = document.getElementById("myInput").value;
     var t = document.createTextNode(inputValue);
+    li.appendChild(checkbox);
     li.appendChild(t);
+    function checkItem(li) {
+        if (li.checked === true)
+            li.nextSibling.style.textDecoration = "line-through";
+        else
+            li.nextSibling.style.textDecoration = "none";
+    }
+
     if (inputValue === '') {
         alert("Пожалуйста добавьте задачу!");
     } else {
         document.getElementById("myUL").appendChild(li);
     }
     document.getElementById("myInput").value = "";
-
     var span = document.createElement("SPAN");
     var txt = document.createTextNode("\u00D7");
     span.className = "close";
     span.appendChild(txt);
     li.appendChild(span);
-
     for (i = 0; i < close.length; i++) {
         close[i].onclick = function() {
             var div = this.parentElement;
